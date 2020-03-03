d1e4a2a174010b01153c7bc2c4de6e062e418d1a
raislansilva <raislan@live.com>
2020-03-03 17:06:58 -0500

    first commit

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/cadastro', 'UsuarioController@cadastro');
+
+Route::post('/login','UsuarioController@login');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('auth:api')->put('/perfil','UsuarioController@perfil');
